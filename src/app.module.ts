22d1de62cbeb7cccce65eae6d2dcc2318c09327d
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-05-23 10:52:31 -0700

    Added in typical analytic metrics.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebTrafficModule } from './web_traffic/web_traffic.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { BounceRatesModule } from './bounce_rates/bounce_rates.module';
+import { UniquePageViewsModule } from './unique_page_views/unique_page_views.module';
+import { NewVisitorsModule } from './new_visitors/new_visitors.module';
+import { ReturningVisitorsModule } from './returning_visitors/returning_visitors.module';
 
 @Module({
-  imports: [],
+  imports: [WebTrafficModule, SessionsModule, BounceRatesModule, UniquePageViewsModule, NewVisitorsModule, ReturningVisitorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
